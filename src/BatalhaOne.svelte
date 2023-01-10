f7c6a0df634649c04d16d5eda52b50a3cd19afb6
Emanoel Carvalho <ehcs@dicente.ifpe.edu.br>
2023-01-10 16:57:14 -0300

    Modificação da Batalha, inserimos o Html e o CSS

--- src/BatalhaOne.svelte
+++ src/BatalhaOne.svelte
@@ -3,8 +3,8 @@ import { estado, trocarestadodojogo } from './Estado';
 import { Protagonista} from '../Protagonista';
 import {Boss1} from '../Liliane'
 import { writable } from 'svelte/store';
-    import Menu from './Menu.svelte';
-    import Jogar from './Jogar.svelte';
+import Menu from './Menu.svelte';
+import Jogar from './Jogar.svelte';
 
 
 let Prota = new Protagonista( "Luiza",118, 11, 5);
@@ -22,18 +22,19 @@ function trocarTurno(JogadaDaVez){
             console.log(JogadaDaVez)
         }, 5000);
     }
-}
+};
 
-function AtaqueProta(){
+function ataqueProta(){
     Prota.ChuvaDeCodigos(BossOne);
     BossOne.Linux(Prota);
+    console.log(Prota.hp, BossOne.hp);
     if(BossOne.hp <= 0){
         console.log(ProximaFase());
     } else if(Prota.hp <= 0){
            return FimDeJogo();
         }
-        console.log(BossOne.hp, Prota.hp);
-    }
+        trocarTurno();
+    };
 
     function ataqueProtaEspecial(){
         if(Prota.hp < 59){
@@ -45,26 +46,26 @@ function AtaqueProta(){
     } else if(Prota.hp <= 0){
            return FimDeJogo();
         }
-    }
+        trocarTurno();
+    };
 
     function curaProtagonista(){
         if(Prota.hp < 59){
             Prota.HoraDoCafe();
             console.log(Prota.hp) ;      
         }
-    }
+        trocarTurno();
+    };
 
-function ProximaFase(){
-    console.log("Protagonista Ganhou essa fase!");
-}
 
-function nomeProtagonista(){
-    console.log(Prota.nome);
-}
-function FimDeJogo(){
-    console.log("Fim De Jogo!");
-    console.log("Você Perdeu!");
-}
+    function ProximaFase(){
+        console.log("Protagonista Ganhou essa fase!");
+    };
+
+    function FimDeJogo(){
+        console.log("Fim De Jogo!");
+        console.log("Você Perdeu!");
+    };
 
 /*function turnos(){
     if($JogadaDaVez == true){
@@ -108,29 +109,80 @@ ataqueBoss(BossOne);*/
 </script>
 
 <main>
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div on:click={() => AtaqueProta()}>
-        <button>
-            ChuvaDeCodigos
-        </button>
+    <div id="container">
+        <div id="topRow">
+            <div id="luizaProta" class="chars">
+                <img src="" alt="" srcset="">
+               
+            </div>
+            <div id="lilianeBoss" class="chars">
+                <img src="" alt="" srcset="">
+            </div>
+        </div>
+        <div id="bottonRow"></div>
     </div>
 
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div on:click={() => curaProtagonista()}>
-        <button>
-            HoraDoCafe
-        </button>
-    </div>
+    
+    <div id="ataques">
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div on:click={() => ataqueProta()}>
+            <button>
+                ChuvaDeCodigos
+            </button>
+        </div>
 
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div on:click={() => ataqueProtaEspecial()}>
-        <button>
-            Recursao
-        </button>
-    </div>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div on:click={() => ataqueProtaEspecial()}>
+            <button>
+                Recursao
+            </button>
+        </div>
 
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div on:click={() => curaProtagonista()}>
+            <button>
+                HoraDoCafe
+            </button>
+        </div>
+    </div>
 </main>
 
 <style>
+#ataques{
+    text-align: justify;
+    float: right;
+    width: 500px;
+}
+/*#topRow{
+    display: flex;
+    flex-direction: row;
+    border: 1px solid;
+    border-color: aqua;
+    justify-content: center;
+}
 
+#luizaProta{
+    border: 1px solid;
+}
+
+#luizaProta img{
+    height: 200px;
+    margin-top: 400px;
+}
+#lilianeBoss{
+    border: 1px solid;
+}
+
+#lilianeBoss{
+    height: 200px;
+    margin-top: 400px;    
+}
+.chars{
+    margin: 15px 40px;
+}
+
+#bottonRow{
+    padding: 20px;
+    font: 20pt;
+}*/
 </style>
