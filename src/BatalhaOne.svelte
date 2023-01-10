2cb5f90d762c44c5cf70eedd9a24d446ae0a4e6d
Emanoel Carvalho <ehcs@dicente.ifpe.edu.br>
2023-01-09 22:28:58 -0300

    Atualizando BatalhaOne Movimentos de ataque

--- src/BatalhaOne.svelte
+++ src/BatalhaOne.svelte
@@ -1,8 +1,10 @@
 <script>
-import { estado } from './Estado';
+import { estado, trocarestadodojogo } from './Estado';
 import { Protagonista} from '../Protagonista';
 import {Boss1} from '../Liliane'
 import { writable } from 'svelte/store';
+    import Menu from './Menu.svelte';
+    import Jogar from './Jogar.svelte';
 
 
 let Prota = new Protagonista( "Luiza",118, 11, 5);
@@ -23,26 +25,86 @@ function trocarTurno(JogadaDaVez){
 }
 
 function AtaqueProta(){
-    trocarTurno();
-    Prota.ChuvaDeCodigos(BossOne)
-    BossOne.Arduino(Prota)
+    Prota.ChuvaDeCodigos(BossOne);
+    BossOne.Linux(Prota);
     if(BossOne.hp <= 0){
+        console.log(ProximaFase());
+    } else if(Prota.hp <= 0){
+           return FimDeJogo();
+        }
+        console.log(BossOne.hp, Prota.hp);
+    }
+
+    function ataqueProtaEspecial(){
+        if(Prota.hp < 59){
+            Prota.Recursao(BossOne);
+            BossOne.Arduino(Prota);
+        }
+        if(BossOne.hp <= 0){
         return ProximaFase();
     } else if(Prota.hp <= 0){
            return FimDeJogo();
         }
     }
 
+    function curaProtagonista(){
+        if(Prota.hp < 59){
+            Prota.HoraDoCafe();
+            console.log(Prota.hp) ;      
+        }
+    }
 
 function ProximaFase(){
-    console.log("Protagonista Ganhou essa fase!")
+    console.log("Protagonista Ganhou essa fase!");
 }
 
-
+function nomeProtagonista(){
+    console.log(Prota.nome);
+}
 function FimDeJogo(){
-    console.log("Fim De Jogo!")
+    console.log("Fim De Jogo!");
+    console.log("Você Perdeu!");
 }
 
+/*function turnos(){
+    if($JogadaDaVez == true){
+        $JogadaDaVez = false;
+        console.log($JogadaDaVez);
+    }else{
+            setTimeout(function(){
+                $JogadaDaVez = true;
+                console.log($JogadaDaVez);
+        }, 6000)
+    }
+}
+
+function ataqueProta(Prota){
+    if(Prota.hp > 0){
+        setTimeout(function(){
+            Prota.ChuvaDeCodigos(BossOne)
+        }, 3000)
+    }else{
+        console.log("Olá")
+    }
+    turnos();
+
+}
+
+function ataqueBoss(BossOne){
+    if(BossOne.hp > 0){
+        setTimeout(function(){
+            BossOne.Linux(Prota)
+            console.log(Prota.hp)
+        }, 3000)
+    }else{
+     console.log("Ola")
+    }
+    turnos();
+}
+
+
+ataqueProta(Prota);
+ataqueBoss(BossOne);*/
 </script>
 
 <main>
@@ -52,6 +114,20 @@ function FimDeJogo(){
             ChuvaDeCodigos
         </button>
     </div>
+
+    <!-- svelte-ignore a11y-click-events-have-key-events 
+    <div on:click={() => curaProtagonista()}>
+        <button>
+            HoraDoCafe
+        </button>
+    </div>-->
+
+    <!-- svelte-ignore a11y-click-events-have-key-events 
+    <div on:click={() => ataqueProtaEspecial()}>
+        <button>
+            Recursao
+        </button>
+    </div>-->
 
 </main>
 
