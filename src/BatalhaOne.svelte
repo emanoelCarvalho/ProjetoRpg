22d63c729031cc87bd04ea9b8780e1df3dc59755
Emanoel Carvalho <ehcs@dicente.ifpe.edu.br>
2023-01-16 17:10:50 -0300

    Alteração na BatallhaOne parte de JavaScript

--- src/BatalhaOne.svelte
+++ src/BatalhaOne.svelte
@@ -8,31 +8,71 @@
     let protaBatalha1 = new Protagonista("Luiza", 118, 11, 5);
     let bossOne = new Boss1("Liliane", 128, 13, 5);
 
-    let JogadaDaVez = writable(false);
+    let jogadaDaVez = writable(false);
 
-    function trocarTurno(JogadaDaVez) {
-        if (JogadaDaVez == false) {
-            JogadaDaVez = true;
-            console.log(JogadaDaVez);
-        } else if (JogadaDaVez == true) {
+    function trocarTurno() {
+        if ($jogadaDaVez == false) {
+            $jogadaDaVez = true;
+            console.log($jogadaDaVez);
+        } else if ($jogadaDaVez == true) {
             setTimeout(function () {
-                JogadaDaVez = false;
-                console.log(JogadaDaVez);
+                $jogadaDaVez = false;
+                console.log($jogadaDaVez);
             }, 5000);
         }
-    };
+    }
 
     function ataqueProta() {
-        protaBatalha1.ChuvaDeCodigos(bossOne);
-        bossOne.Linux(protaBatalha1);
-        console.log(protaBatalha1.hp, bossOne.hp);
-        if (bossOne.hp <= 0) {
-            console.log(proximaFase());
-        } else if (protaBatalha1.hp <= 0) {
-            return bossGanhouJogo();
+        let dado10 = Math.round(Math.random() * 10);
+        if (dado10 % 2 == 0) {
+            console.log(protaBatalha1.nome + " Usou Chuva de códigos");
+            setTimeout(function () {
+                protaBatalha1.ChuvaDeCodigos(bossOne);
+                console.log(protaBatalha1.hp, bossOne.hp);
+            }, 2000);
+        } else {
+            setTimeout(function () {
+                console.log(protaBatalha1.nome + " Errou o ataque");
+            }, 2000);
+        }
+        setTimeout(function () {
+            if (bossOne.hp <= 0) {
+                setTimeout(function () {
+                    proximaFase();
+                }, 2000);
+            }else {
+        setTimeout(function () {
+            bossAtaque();
+        }, 3000);
+        trocarTurno();
+            }
+        }, 2000);
+    }
+
+    function bossAtaque() {
+        let dado10 = Math.round(Math.random() * 10);
+        if (dado10 % 2 != 0) {
+            console.log(bossOne.nome + " Usou Linux ataque");
+            setTimeout(function () {
+                aleatorioAtaque();
+            }, 2000);
+        }
+    }
+
+    function aleatorioAtaque() {
+        let dado12 = Math.round(Math.random() * 12);
+        if (dado12 > 7) {
+            setTimeout(function () {
+                bossOne.Linux(protaBatalha1);
+                console.log(protaBatalha1.hp, bossOne.hp);
+            }, 2000);
+        } else {
+            setTimeout(function () {
+                console.log(bossOne.nome + " Errou o ataque");
+            }, 2000);
         }
         trocarTurno();
-    };
+    }
 
     function ataqueProtaEspecial() {
         if (protaBatalha1.hp < 59) {
@@ -45,7 +85,7 @@
             return bossGanhouJogo();
         }
         trocarTurno();
-    };
+    }
 
     function curaProtagonista() {
         if (protaBatalha1.hp < 59) {
@@ -53,7 +93,7 @@
             console.log(protaBatalha1.hp);
         }
         trocarTurno();
-    };
+    }
 
     function proximaFase() {
         console.log("Protagonista Ganhou essa fase!");
@@ -63,17 +103,17 @@
         blocoTwo.style.visibility = "hidden";
 
         trocarestadodojogo("menu");
-    };
+    }
 
     function bossGanhouJogo() {
         // @ts-ignore
         blocoOne.style.visibility = "hidden";
         // @ts-ignore
         blocoTwo.style.visibility = "hidden";
-        
+
         console.log("Você Perdeu!");
         trocarestadodojogo("menu");
-    };
+    }
 
     function iniciarLuta() {
         // @ts-ignore
@@ -82,7 +122,7 @@
         blocoTwo.style.visibility = "visible";
         // @ts-ignore
         iniciandoBatle.style.visibility = "hidden";
-    };
+    }
 </script>
 
 <main>
@@ -144,7 +184,6 @@
         width: 100vw;
         height: 100vh;
         flex-direction: row;
-        position: absolute;
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
