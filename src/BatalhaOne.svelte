f229079f884aa9ebddb58ab674876bed773ca29e
Emanoel Carvalho <ehcs@dicente.ifpe.edu.br>
2023-01-17 00:08:39 -0300

    Atualização do script de batalha, adicionei barra de vida dos personagens

--- src/BatalhaOne.svelte
+++ src/BatalhaOne.svelte
@@ -18,60 +18,66 @@
             setTimeout(function () {
                 $jogadaDaVez = false;
                 console.log($jogadaDaVez);
-            }, 5000);
+            }, 1000);
         }
     }
 
     function ataqueProta() {
-        let dado10 = Math.round(Math.random() * 10);
-        if (dado10 % 2 == 0) {
+        let dado3 = Math.round(Math.random() * 3);
+        if (dado3 % 2 == 0) {
             console.log(protaBatalha1.nome + " Usou Chuva de códigos");
             setTimeout(function () {
                 protaBatalha1.ChuvaDeCodigos(bossOne);
                 console.log(protaBatalha1.hp, bossOne.hp);
-            }, 2000);
+            }, 1000);
         } else {
             setTimeout(function () {
                 console.log(protaBatalha1.nome + " Errou o ataque");
-            }, 2000);
+            }, 1000);
         }
         setTimeout(function () {
             if (bossOne.hp <= 0) {
                 setTimeout(function () {
                     proximaFase();
-                }, 2000);
-            }else {
-        setTimeout(function () {
-            bossAtaque();
-        }, 3000);
-        trocarTurno();
+                }, 1000);
+            } else {
+                setTimeout(function () {
+                    bossAtaque();
+                }, 1000);
+                trocarTurno();
             }
-        }, 2000);
+        }, 1000);
     }
 
     function bossAtaque() {
-        let dado10 = Math.round(Math.random() * 10);
-        if (dado10 % 2 != 0) {
-            console.log(bossOne.nome + " Usou Linux ataque");
+        let dado3 = Math.round(Math.random() * 3);
+        if (dado3 % 2 != 0) {
             setTimeout(function () {
                 aleatorioAtaque();
-            }, 2000);
+            }, 1000);
+        } else {
+            setTimeout(function () {
+                console.log(bossOne.nome + " Errou o ataque");
+            }, 1000);
         }
+        trocarTurno();
     }
 
     function aleatorioAtaque() {
         let dado12 = Math.round(Math.random() * 12);
-        if (dado12 > 7) {
+        if (dado12 >= 7) {
             setTimeout(function () {
+                console.log(bossOne.nome + " Usou Linux ataque");
                 bossOne.Linux(protaBatalha1);
                 console.log(protaBatalha1.hp, bossOne.hp);
-            }, 2000);
+            }, 1000);
         } else {
             setTimeout(function () {
-                console.log(bossOne.nome + " Errou o ataque");
-            }, 2000);
+                console.log(bossOne.nome + " Usou Arduíno ataque");
+                bossOne.Arduino(protaBatalha1);
+                console.log(protaBatalha1.hp, bossOne.hp);
+            }, 1000);
         }
-        trocarTurno();
     }
 
     function ataqueProtaEspecial() {
@@ -92,7 +98,6 @@
             protaBatalha1.HoraDoCafe();
             console.log(protaBatalha1.hp);
         }
-        trocarTurno();
     }
 
     function proximaFase() {
@@ -134,9 +139,10 @@
             </div>
             <div id="blocoOne">
                 <div class="protaInformaçoes">
+                    <div id="barraDeVidaProta" />
+                    <div id="barraDeEstamina" />
                     <p class="nomeProta">{protaBatalha1.nome}</p>
                     <p class="nomeProta">{protaBatalha1.hp}</p>
-                    <div id="barraDeVidaProta" />
                 </div>
                 <div id="prota">
                     <img id="protaImagem" src="" alt="" srcset="" />
@@ -161,6 +167,8 @@
 
             <div id="blocoTwo">
                 <div class="liliInfo">
+                    <div id="barraDeVidaBoss" />
+                    <div id="barraDeEstaminaBoss" />
                     <p class="nomeBoss">{bossOne.nome}</p>
                     <p class="nomeBoss">{bossOne.hp}</p>
                 </div>
@@ -192,9 +200,42 @@
         float: right;
         visibility: hidden;
     }
+    #barraDeVidaProta {
+        width: 100%;
+        height: 20px;
+        background-color: greenyellow;
+        position: relative;
+        top: 0;
+        left: 0;
+    }
+    #barraDeEstamina {
+        width: 100%;
+        height: 10px;
+        background-color: cyan;
+        position: relative;
+        top: 0;
+        left: 0;
+    }
     #blocoTwo {
         float: left;
         visibility: hidden;
+    }
+    #barraDeVidaBoss {
+        width: 100%;
+        height: 20px;
+        background-color: greenyellow;
+        position: relative;
+        top: 0;
+        right: 0;
+    }
+
+    #barraDeEstaminaBoss {
+        width: 100%;
+        height: 10px;
+        background-color: cyan;
+        position: relative;
+        top: 0;
+        right: 0;
     }
     #boss {
         height: 400px;
